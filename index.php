e1b88d100df8070975f9967000052ded01173710
Rafael Amaral <rafaelsantosdoamaral07@gmail.com>
2024-09-05 14:02:12 -0300

    Função menu inicial implementada

--- index.php
+++ index.php
@@ -33,8 +33,15 @@
         $login = readline("Digite seu novo nome de usuário: ");
         $senha = readline("Digite sua nova senha: ");
 
+        if (isset($usuario[$login])) {
+            echo "Usuário já existe.\n";
+            return;
+        }
+
         $usuario[$login] = $senha;
         echo "Usuário registrado com sucesso!";
+        $data = date('d/m/Y H:i:s');
+        addLog("Usuario $login registrado as $data");
     }
 
     function fazerVendas() {
@@ -42,12 +49,37 @@
 
         $nomeProduto = readline("Digite o nome do produto: ");
         $valorProduto = readline("Digite o valor do produto: ");
+
+        $vendas[$nomeProduto] = $valorProduto;
+        addLog("Produto vendido");
     }
 
-    login();
-    login();
-    registrar();
-    
+    function menuInicial($usuario) {
+        echo "Bem-vindo ao sistema de gerenciamento de caixa\n";
+        echo "1. Login\n";
+        echo "2. Cadastrar novo usuário\n";
+        
+        $option = readline("Escolha uma opção: ");
+        print_r($usuario);
+
+        switch($option) {
+            case 1:
+                login($usuario);
+                break;
+            case 2:
+                registrar();
+                break;
+        }
+    }
+
+    function addLog($log) {
+        global $logs;
+
+        $logs[] = $log;
+    }
+    while (true){
+    menuInicial($usuario);
+}
 
 
 
